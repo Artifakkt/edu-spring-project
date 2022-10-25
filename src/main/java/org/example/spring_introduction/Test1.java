49810e033ee228b53393f773e1ace1788b0f5f07
TetianaRzhavska <tetianarzhavska@gmail.com>
2022-10-25 16:54:03 +0300

    First commit

---
+++ src/main/java/org/example/spring_introduction/Test1.java
@@ -0,0 +1,14 @@
+package org.example.spring_introduction;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.say();
+
+        Pet pet = new Dog();
+        pet.say();
+
+        Cat cat = new Cat();
+        cat.say();
+    }
+}
